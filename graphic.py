001f5312891a8a68aebef7bd92f2b838ae44a058
varunhari <47211645+varunhari@users.noreply.github.com>
2020-06-15 18:52:39 +0530

    Add files via upload

---
+++ graphic.py
@@ -0,0 +1,45 @@
+import kivy 
+from kivy.app import App
+from kivy.uix.label import Label 
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Rectangle
+from kivy.graphics import Color 
+from kivy.graphics import Line 
+
+
+
+class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)  
+
+        with self.canvas:
+            Line(points=(20,30, 400,500, 60,500))
+            Color(1,0,0, .5, mode='rgba') 
+            self.rect = Rectangle(pos=(0,0), size=(50,50)) 
+
+
+
+    def on_touch_down(self, touch):
+        self.rect.pos = touch.pos 
+        print("Mouse Down", touch)
+
+
+    def on_touch_move(self, touch):
+        self.rect.pos = touch.pos 
+        print("Mouse Move", touch)
+ 
+
+
+
+class MyApp(App):
+    def build(self):
+        return Touch() 
+
+
+if __name__ == "__main__":
+    MyApp().run() 
